08d0356f9b52f985f7fd8e1ba90174cd9b09ac87
TecHeReTiC3141 <talex73@mail.ru>
2026-03-20 00:43:41 +0300

    fix lint

--- advanced-lab-2-pg-backup/scripts/task2/backup-restore.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-latest_backup=$(ls -t ~/backups/dumps/*.sql.gz | head -1)
-unpacked_file="${latest_backup%.gz}"
-
-gzip -d $latest_backup
-# sed -i '' 's|/var/db/postgres1/idd21|/var/db/postgres2/idd21|g' $unpacked_file
-# sed -i '' 's|/var/db/postgres1/gzp28|/var/db/postgres2/gzp28|g' $unpacked_file
-psql -h localhost -p 9052 -U postgres1 -d postgres -f $unpacked_file
